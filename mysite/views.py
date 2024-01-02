3a4660cea59d63e243fd4307c89b478bc2c7b4f9
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-01-03 03:48:09 +0600

    implement django template

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('<h3>Hello, This is Sohan</h3>');
+    #return HttpResponse('<h3>Hello, This is Sohan</h3>');
+    return render(request,"home.html")
